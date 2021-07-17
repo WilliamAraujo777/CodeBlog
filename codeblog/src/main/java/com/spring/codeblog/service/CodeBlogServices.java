7f5815819e06aa3aab1a469f2d8c64bb9112a58f
William Araujo <will.araujo777@gmail.com>
2021-07-17 01:18:05 -0300

    Update CodeBlogServices.java

--- codeblog/src/main/java/com/spring/codeblog/service/CodeBlogServices.java
+++ codeblog/src/main/java/com/spring/codeblog/service/CodeBlogServices.java
@@ -6,17 +6,10 @@ import org.springframework.stereotype.Service;
 
 import com.spring.codeblog.model.Publicacao;
 
-@Service
 public interface CodeBlogServices {
 	
-	
 	List<Publicacao> findAll ();
-	
 	Publicacao findById(long id);
-	
 	Publicacao save(Publicacao pub);
-	
-	
-	
 	
 }
