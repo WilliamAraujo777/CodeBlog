eb8a260988e60ac717280b2794e774f9896e17f0
William Araujo <will.araujo777@gmail.com>
2021-07-18 13:10:32 -0300

    adicionando publicacoes a pagina
    Utilizando thymeleaf, adicionei dados da publicação na página html

--- codeblog/src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ codeblog/src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.spring.codeblog.model.Publicacao;
 import com.spring.codeblog.service.CodeBlogServices;
 
@@ -20,6 +20,14 @@ public class CodeBlogController {
 	
 	@RequestMapping(value = "/pubs", method = RequestMethod.GET)
 	public ModelAndView getPubs() {
+		ModelAndView mv = new ModelAndView("pubs");
+		List<Publicacao> pubs = codeBlogServices.findAll();
+		mv.addObject("pubs", pubs);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/pubs/{id}", method = RequestMethod.GET)
+	public ModelAndView getPubDetails(@PathVariable("id") long id) {
 		ModelAndView mv = new ModelAndView("pubs");
 		List<Publicacao> pubs = codeBlogServices.findAll();
 		mv.addObject("pubs", pubs);
