1973cc13e2e8b42b65d8d606d1c8e214781a1f51
William Araujo <will.araujo777@gmail.com>
2021-07-18 15:25:34 -0300

    adicionando formulário e metodo para publicar

--- codeblog/src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ codeblog/src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -1,14 +1,20 @@
 package com.spring.codeblog.controller;
 
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.spring.codeblog.model.Publicacao;
 import com.spring.codeblog.service.CodeBlogServices;
 
@@ -37,5 +43,17 @@ public class CodeBlogController {
 	@RequestMapping(value = "/newpub", method = RequestMethod.GET)
 	public String getPubForm() {
 		return "pubForm";
+	}
+	
+	@RequestMapping(value = "/newpub", method = RequestMethod.POST)
+	public String savePost(@Valid Publicacao pub, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			return "redirect:/newpub";
+		}
+		
+		pub.setData_pub(LocalDate.now());
+		codeBlogServices.save(pub);
+		return "redirect:/pubs";
+		
 	}
 }
